421e5e6d8899ce5e7c512b3e462694ed64aa633a
Adilson Krischanski <69815586+adilsonkrischanski@users.noreply.github.com>
2021-09-30 18:19:32 -0300

    Delete PYTHON directory

--- PYTHON/EXERCICE/72.py
+++
@@ -1,7 +0,0 @@
-numerbers = ('zero','um','dois','tres','quatro','cinco')
-
-a=-1
-while a<0 or a>5:
-    a= int(input("enter a number enter 0 and 5: "))
-
-print(numerbers[a])
--- PYTHON/EXERCICE/74.py
+++
@@ -1,4 +0,0 @@
-from random import randrange
-a = (randrange(1,10),randrange(1,10),randrange(1,10),randrange(1,10),randrange(1,10)) #randomizador
-print(a)
-print(f'O maior numero é {sorted(a)[-1]} e o menor é {sorted(a)[0]}')
